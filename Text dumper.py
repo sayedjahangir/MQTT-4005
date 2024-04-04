4011557c4d5be3cc909fd688be3b83fa7d620529
abdiraufmoha05 <166077003+abdiraufmoha05@users.noreply.github.com>
2024-04-05 00:24:11 +0100

    Update Text dumper.py
    Text file name contains date and time to prevent overwriting.

--- Text dumper.py
+++ Text dumper.py
@@ -1,3 +1,6 @@
+import datetime
+
 def text_dump(input):
-    with open("error.txt", 'w') as x:
-        x.write(input)
+    error_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S") # names the file after the current date and time to prevent duplicates
+    with open(f"error{error_time}.txt", 'w') as x:
+        x.write(input) # writes information on the error
