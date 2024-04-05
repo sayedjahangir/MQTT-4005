f9d651b0d38b9d3a0c40789b725b6c9494448e7f
sayedjahangir <166069110+sayedjahangir@users.noreply.github.com>
2024-04-05 01:20:31 +0100

    Update subscribe.py
    Interfaces with other files.

--- subscribe.py
+++ subscribe.py
@@ -1,15 +1,16 @@
 import paho.mqtt.client as mqtt
 from data_to_DB import RecieveData
 
-MQTT_Broker = "localhost"
-MQTT_Port = 1883
-Keep_Alive_Interval = 40
+from broker import broker
+from rsa_encryption import decrypt
+
 MQTT_Topic = "Home/#"
 
 def on_connect(mosq, obj, flags, rc):
 	mqttc.subscribe(MQTT_Topic, 0)
 
 def on_message(mosq, obj, msg):
+    msg.payload = decrypt(msg.payload)
     print("MQTT data received...")
     print(f"MQTT Topic: {str(msg.topic)}.")
     print(f"Message: {str(msg.payload)}.")
@@ -24,6 +25,9 @@ mqttc.on_message = on_message
 mqttc.on_connect = on_connect
 mqttc.on_subscribe = on_subscribe
 
-mqttc.connect(MQTT_Broker, int(MQTT_Port), int(Keep_Alive_Interval))
+try:
+    mqttc.connect(str(broker.mqtt_broker), int(broker.mqtt_port), int(broker.keep_alive_interval))
+except:
+    print("Error: invalid configuration.")
 
 mqttc.loop_forever()
